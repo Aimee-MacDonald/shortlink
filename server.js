a6273c3432ed37f8cf8fd00fa60009ce44fcba07
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-08-14 21:53:09 +0200

    Short URL Creation

--- server.js
+++ server.js
@@ -3,21 +3,34 @@ var mongo = require("mongodb").MongoClient;
 var app = express();
 var portNumber = 8000;
 var dburl = "mongodb://admin:password@ds023593.mlab.com:23593/shortlink";
+var database;
+
+mongo.connect(dburl, function(err, db){
+  if(err)throw err;
+  database = db;
+
+  app.listen(portNumber, function(){
+    console.log("Server Started Listening on Port: " + portNumber);
+  });
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.get("/get-link/:longLink", function(req, res){
   var longLink = req.params.longLink;
+  var shortLink;
+  var collection = database.collection("links");
 
-  mongo.connect(dburl, function(err, db){
-    if(err)throw err;
-    db.collection("links").count({}, function(err, cnt){
-      if(err)throw err;
-      shortLink = "https://sl.glitch.me/" + (cnt+1).toString(36);
-      res.status(200).send({link: shortLink});
-      storeLink(longLink, shortLink);
-    });
-    db.close();
+  collection.count({}, function(err, cnt){
+    shortLink = "https://sl.glitch.me/" + (cnt+1).toString(36);
+
+    var respac = {
+      longlink: longLink,
+      shortlink: shortLink
+    };
+
+    res.status(200).send(respac);
+    collection.insertOne(respac);
   });
 });
 
@@ -27,16 +40,4 @@ app.get("/get-link/:longLink", function(req, res){
 */
 app.get("/:shortLink", function(req, res){
   res.redirect("https://google.com");
-});
-
-function storeLink(ll, sl){
-  var l = {
-    longlink: ll,
-    shortlink: sl
-  }
-  console.log(l);
-}
-
-app.listen(portNumber, function(){
-  console.log("Server Started Listening on Port: " + portNumber);
 });

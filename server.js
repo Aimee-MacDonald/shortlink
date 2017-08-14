e7ed673a013f9a7256f2063ad1540eea2e1e04b8
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-08-14 14:11:24 +0200

    Serve HTML Homepage

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var portNumber = 8000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(portNumber, function(){
   console.log("Server Started Listening on Port: " + portNumber);
 });

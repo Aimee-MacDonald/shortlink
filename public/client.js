9b114d2a266a170c84b1fead9802717d6d33ae16
System <none>
2017-08-26 14:24:19 +0000

    👜💣 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,63 +1,55 @@
-var apiF = false;
-
-var el_apibox = document.getElementById("apibox");
 var el_textbox = document.getElementById("textbox");
 var el_arrow = document.getElementById("arrow");
+var el_apibox = document.getElementById("apibox");
+
+var apiFlag = false;
+
+// When the Window is Loaded
+window.onload = function(){
+  console.log("Hi");
+  // Handle form Submit
+  document.getElementById("urlwidget").onsubmit = function(){
+    var longlink = el_textbox.value;
+    
+    el_arrow.className = "spinner";
+    
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://sl.glitch.me/get-link?longlink=" + longlink);
+    
+    request.onload = function(){
+      var data = JSON.parse(request.responseText);
+      el_textbox.value = data.link;
+      el_arrow.className = "arrow";
+    };
+    
+    request.onerror = function(){
+      el_textbox.value = "Oups!";
+      el_arrow.className = "arrow";
+    };
+    
+    request.send();
+    
+    return false;
+  };
+};
 
 document.addEventListener("click", function(e){
-  var t = e.target.id;
-
-  switch (t) {
+  switch(e.target.id){
     case "apibtn":
     case "apit":
       toggleAPI();
       break;
-
+      
     case "textbox":
-      el_textbox.value = "";
-      break;
-
-    case "gobutton":
-    case "arrow":
-      createLink();
-      break;
-
-    default:
-    console.log(t);
+      if(el_textbox.value === "Shorten any Link!")el_textbox.value = "";
   }
 });
 
 function toggleAPI(){
-  apiF = !apiF;
-  if(apiF){
+  apiFlag = !apiFlag;
+  if(apiFlag){
     el_apibox.style.display = "block";
   } else {
     el_apibox.style.display = "none";
   }
-}
-
-function createLink(){
-  var longlink = el_textbox.value;
-
-  if(longlink.slice(0, 7) === "http://")longlink = longlink.slice(7, longlink.length);
-  if(longlink.slice(0, 8) === "https://")longlink = longlink.slice(8, longlink.length);
-
-  el_arrow.className = "spinner";
-
-  // Sending the Request
-  var request = new XMLHttpRequest();
-  request.open("GET", "https://sl.glitch.me/get-link/" + longlink);
-
-  request.onload = function(){
-    var data = JSON.parse(request.responseText);
-    el_textbox.value = data.link;
-    el_arrow.className = "arrow";
-  };
-
-  request.onerror = function(){
-    console.log("Oups!");
-    el_arrow.className = "arrow";
-  };
-
-  request.send();
 }
